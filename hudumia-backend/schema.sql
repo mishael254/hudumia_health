72e82c55c418f9eb9bd12fb9a0a651d867e81b1d
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-24 21:54:16 +0300

    include firstname, second name and user name in the table schema

--- hudumia-backend/schema.sql
+++ hudumia-backend/schema.sql
@@ -1,7 +1,9 @@
 --creating a table to store doctors details
 CREATE TABLE doctors (
   id SERIAL PRIMARY KEY,
-  full_name VARCHAR(100) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  second_name VARCHAR(30) NOT NULL,
+  user_name VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
   phone_number VARCHAR(20) UNIQUE,
   password_hash TEXT NOT NULL,
