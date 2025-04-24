b3641dad3faf2cc95729017415b375e0d27e44e2
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-24 17:51:16 +0300

    create a schema file to help with table creation

--- hudumia-backend/serverside.js
+++ hudumia-backend/serverside.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 3004;
+const pool = require('./hudumiadb');
 
 // Middleware
 app.use(cors()); // allow frontend to access backend
@@ -17,4 +18,14 @@ app.get("/api/health-check", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});
+//test the db connectivity
+app.get('/api/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ time: result.rows[0].now });
+  } catch (err) {
+    console.error('Database error:', err);
+    res.status(500).json({ error: 'Database connection failed' });
+  }
 });
