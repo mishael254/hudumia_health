54b6ff0c0cd3a5e6f3d28a713f5e9137ca4ec722
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-24 18:41:34 +0300

    remove secretkey handling constant.. bad implementation

--- hudumia-backend/utils/2fa.js
+++ hudumia-backend/utils/2fa.js
@@ -11,12 +11,6 @@ const generate2FASecret = async (username) => {
   return { ascii: secret.ascii, qrCode };
 };
 
-const verify2FAToken = (secret, token) => {
-  return speakeasy.totp.verify({
-    secret,
-    encoding: 'ascii',
-    token,
-  });
-};
+
 
 module.exports = { generate2FASecret, verify2FAToken };
