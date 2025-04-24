8a2140c1a3cb0ee00e461c220733e64121321308
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-24 16:28:59 +0300

    set up the backend express, and forward a new port to test if the app is running successfully

---
+++ huduma-backend/serverside.js
@@ -0,0 +1,20 @@
+// huduma-backend/serverside.js
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3004;
+
+// Middleware
+app.use(cors()); // allow frontend to access backend
+app.use(express.json()); // allow JSON parsing
+
+// Sample route
+app.get('/api/health-check', (req, res) => {
+  res.json({ status: 'Huduma backend is running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
