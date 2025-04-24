60b6126d08f9dedcddeb5a757fbc34ba58d70cf3
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-24 18:44:06 +0300

    remodel function verify2FAToken Which takes now the secret key generated and verifies if the token is valid using time stamp

--- hudumia-backend/utils/2fa.js
+++ hudumia-backend/utils/2fa.js
@@ -11,6 +11,12 @@ const generate2FASecret = async (username) => {
   return { ascii: secret.ascii, qrCode };
 };
 
-
+const verify2FAToken = (secret, token) => {
+  return speakeasy.totp.verify({
+    secret,
+    encoding: 'ascii',
+    token,
+  });
+};
 
 module.exports = { generate2FASecret, verify2FAToken };
