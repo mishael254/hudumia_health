ea41397f6fc289dc3284b29a054577151eca533e
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-25 17:59:58 +0300

    adjust paths in routes accordingly

--- src/routing/Routes.js
+++ src/routing/Routes.js
@@ -1,9 +1,9 @@
 // src/Routes.js
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import DoctorSignUp from './components/auth/DoctorSignUp'; // Adjust path if needed
-import DoctorLogin from './components/auth/DoctorLogin';   // Adjust path if needed
-import Dashboard from './components/dashboard/Dashboard';     // Adjust
+import DoctorSignUp from '../components/auth/DoctorSignUp';// Adjust path if needed
+import DoctorLogin from '../components/auth/DoctorLogIn';   // Adjust path if needed
+import Dashboard from '../components/dashboard/Dashboard';     // Adjust
 
 const AppRoutes = ({ isAuthenticated, setIsAuthenticated }) => {
     return (
