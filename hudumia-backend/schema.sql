1316223d740c3a8c86ea6f6b712d11b7f15f9df8
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-25 14:35:29 +0300

    insert new columns that will be responsible for storing password reset tokens

--- hudumia-backend/schema.sql
+++ hudumia-backend/schema.sql
@@ -7,6 +7,8 @@ CREATE TABLE doctors (
   email VARCHAR(100) UNIQUE NOT NULL,
   phone_number VARCHAR(20) UNIQUE,
   password_hash TEXT NOT NULL,
+  reset_token VARCHAR(255),
+  reset_token_expiry TIMESTAMP,
   two_fa_enabled BOOLEAN DEFAULT FALSE,
   two_fa_secret TEXT, -- used if 2FA is enabled
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
