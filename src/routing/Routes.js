55f1f7ec9ddddcf6409ea4165472d57b35b63840
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-27 05:18:19 +0300

    make landingpage become the route directory /entry of the app

--- src/routing/Routes.js
+++ src/routing/Routes.js
@@ -4,17 +4,18 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import DoctorSignUp from '../components/auth/DoctorSignUp';// Adjust path if needed
 import DoctorLogin from '../components/auth/DoctorLogIn';   // Adjust path if needed
 import Dashboard from '../components/dashboard/Dashboard';     // Adjust
-
+import LandingPage from '../components/navbars/home/LandingPage';
 const AppRoutes = ({ isAuthenticated, setIsAuthenticated }) => {
     return (
         <Routes>
+            <Route path="/" element={<LandingPage/>} />
             <Route path="/signup" element={<DoctorSignUp />} />
             <Route path="/login" element={<DoctorLogin setIsAuthenticated={setIsAuthenticated} />} />
             <Route
                 path="/dashboard"
                 element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
             />
-            <Route path="/" element={<Navigate to="/login" />} />
+            
         </Routes>
     );
 };
