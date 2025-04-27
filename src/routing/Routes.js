9673eda72567ef716bb954f30c64d81243070eba
Mishael Ndegwa <mishaelmishantoz@gmail.com>
2025-04-27 10:26:48 +0300

    add new route to manage clients

--- src/routing/Routes.js
+++ src/routing/Routes.js
@@ -5,6 +5,7 @@ import DoctorSignUp from '../components/auth/DoctorSignUp';// Adjust path if nee
 import DoctorLogin from '../components/auth/DoctorLogIn';   // Adjust path if needed
 import Dashboard from '../components/dashboard/Dashboard';     // Adjust
 import LandingPage from '../components/navbars/home/LandingPage';
+import ClientDashboard from '../components/dashboard/components/clients/ClientDashboard';
 const AppRoutes = ({ isAuthenticated, setIsAuthenticated }) => {
     return (
         <Routes>
@@ -18,6 +19,7 @@ const AppRoutes = ({ isAuthenticated, setIsAuthenticated }) => {
                 path="/dashboard"
                 element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
             />
+            <Route path="/clients" element={<ClientDashboard />} />
             
         </Routes>
     );
